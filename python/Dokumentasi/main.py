c57937e8618b31dd5dde6abfa3a48a7b94cf8b0c
Ekal <haikal.tebe2412@gmail.com>
2025-08-31 22:39:35 +0700

    Operator ini nge

---
+++ python/Dokumentasi/main.py
@@ -0,0 +1,11 @@
+class Pagar:
+    """kelas pagar untuk membuat objek pagar. Dibuat oleh petani kode sebagai contoh sahaja"""
+    def __init__(self, warna, tinggi, bahan):
+        self.warna = warna
+        self.tinggi = tinggi
+        self.bahan = bahan
+
+print (Pagar.__doc__)
+input('\n tekan [enter] untuk melihat bantuan (dokumentasi) kelas :')
+help (Pagar)
+
