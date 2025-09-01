c7d0bf31faca727f756d6b2893bcabd6f84dc1a0
Ekal <haikal.tebe2412@gmail.com>
2025-09-01 21:46:00 +0700

    Learning_Project

--- python/Operator/Logika.py
+++ python/Operator/Logika.py
@@ -5,11 +5,22 @@
 a = True
 b = False
 
-c = a and b
-print ("%r and %r = %r" %(a,b,c))
+#Logika and Operasi ini hanya menghasilkan hasil benar jika kedua nilai tersebut benar.
+# c = a and b
+# print ("%r and %r = %r" %(a,b,c))
 
-c = a or b
-print ("%r or %r = %r" %(a,b,c))
+# c = a or b
+# print ("%r or %r = %r" %(a,b,c))
 
-c = not a
-print ("%r not : %r" % (a,c))
+# c = not a
+# print ("%r not : %r" % (a,c))
+
+options = True, False 
+for a in options:
+    for b in options:
+        print (f"{a} and {b} gives {a and b}")
+
+names = "James", "Bob", "David", "Jane", "Kate", "Mary"
+for name in names:
+   if len(name) < 5 and name[0] == "J":
+      print(name)
