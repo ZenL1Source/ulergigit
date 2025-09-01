849c1e4c6307037a761feab8dac81759545d17bd
Ekal <haikal.tebe2412@gmail.com>
2025-09-01 08:15:47 +0700

    Learning_Project

---
+++ python/Operator/Logika.py
@@ -0,0 +1,15 @@
+#Operator logika 
+# digunakan untuk membuat operasi logika, seperti 
+# logika AND, OR, dan NOT (negasi/kebalikan).
+
+a = True
+b = False
+
+c = a and b
+print ("%r and %r = %r" %(a,b,c))
+
+c = a or b
+print ("%r or %r = %r" %(a,b,c))
+
+c = not a
+print ("%r not : %r" % (a,c))
